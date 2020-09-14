253773d2a1367bea79f05f95b5e2aaeec0f73fba
Amit Kumar <amitrke@gmail.com>
2020-09-14 11:08:55 -0400

    Google Signin

--- ngapp/src/app/app.module.ts
+++ ngapp/src/app/app.module.ts
@@ -4,15 +4,34 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
+const config = {
+  apiKey: "AIzaSyAgvZh2TZUc_n2dvu0oOo6tUgA1nJzEkwM",
+  authDomain: "myrke-189201.firebaseapp.com",
+  databaseURL: "https://myrke-189201.firebaseio.com",
+  projectId: "myrke-189201",
+  storageBucket: "myrke-189201.appspot.com",
+  messagingSenderId: "670134176077",
+  appId: "1:670134176077:web:9ee500127a2e0e0b558f04",
+  measurementId: "G-Z48Q0SRCJB"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
