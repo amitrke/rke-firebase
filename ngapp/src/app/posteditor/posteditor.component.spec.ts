eb8b737c04c97e086a6eb66eccab5347240c860d
Amit Kumar <amitrke@gmail.com>
2020-11-15 15:17:59 -0500

    Angular version update step 1

--- ngapp/src/app/posteditor/posteditor.component.spec.ts
+++ ngapp/src/app/posteditor/posteditor.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PosteditorComponent } from './posteditor.component';
 
@@ -6,7 +6,7 @@ describe('PosteditorComponent', () => {
   let component: PosteditorComponent;
   let fixture: ComponentFixture<PosteditorComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PosteditorComponent ]
     })
