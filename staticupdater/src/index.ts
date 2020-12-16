d676f2685c450a22403a1e680c9de77ff4f20f4d
Amit Kumar <amitrke@gmail.com>
2020-12-16 08:55:55 -0500

    Update static to connect to DB

--- staticupdater/src/index.ts
+++ staticupdater/src/index.ts
@@ -1,17 +1,23 @@
-function resolveAfter2Seconds() {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve('resolved');
-      }, 2000);
-    });
-  }
-  
-  async function asyncCall() {
-    console.log('calling');
-    const result = await resolveAfter2Seconds();
-    console.log(result);
-    // expected output: "resolved"
-  }
-  
-  asyncCall();
-  
+import * as admin from 'firebase-admin';
+var serviceAccount = require("../firebase-adminsdk.json");
+
+var app = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://myrke-189201.firebaseio.com"
+});
+
+var db = admin.database();
+
+async function readPosts(){
+  var ref = db.ref("posts");
+  const snapshot = await ref.once('value');
+  return snapshot.val();
+}
+
+async function asyncCall() {
+  const posts = await readPosts();
+  console.log(posts);
+  app.delete();
+}
+
+asyncCall();
