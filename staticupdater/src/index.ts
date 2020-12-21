fb1832c7d1d49f258fcb7c305a0966e09b8ae23d
Amit Kumar <amitrke@gmail.com>
2020-12-20 21:44:23 -0500

    Handlebars templates

--- staticupdater/src/index.ts
+++ staticupdater/src/index.ts
@@ -1,5 +1,8 @@
 import * as admin from 'firebase-admin';
 var serviceAccount = require("../firebase-adminsdk.json");
+var _ = require('lodash');
+const handlebars = require("handlebars");
+const fs = require('fs');
 
 var app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -14,9 +17,42 @@ async function readPosts(){
   return snapshot.val();
 }
 
+async function readUsers(){
+  var ref = db.ref("users");
+  const snapshot = await ref.once('value');
+  return snapshot.val();
+}
+
+function processPosts(posts: any, users: any): any[]{
+  const newPosts:any[] = [];
+  _.forEach(posts, (userPosts:any, userId: string) => {
+    _.forEach(userPosts, (post: any, postId: string) => {
+      console.log(`-${users[userId].name} -- ${postId}----------------`);
+      if (post.publish && post.publish === true){
+        post.userName = users[userId].name;
+        newPosts.push(post);
+      }
+    })
+  })
+  return newPosts;
+}
+
+function generateFiles(posts: any) {
+  const source = fs.readFileSync("./src/post.hbs", 'utf8');
+  const template = handlebars.compile(source, { strict: true });
+  console.log(template);
+  posts.forEach( (post:any) => {
+    const result = template(post);
+    const fileName = post.title.split(' ').join('-');
+    fs.writeFileSync(`./output/${fileName}.html`, result);
+  });
+}
+
 async function asyncCall() {
   const posts = await readPosts();
-  console.log(posts);
+  const users = await readUsers();
+  const userPosts = processPosts(posts, users);
+  generateFiles(userPosts);
   app.delete();
 }
 
