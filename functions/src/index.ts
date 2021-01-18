ad6ff1e797561c013ad15bc264e23205f43d6617
Amit Kumar <amitrke@gmail.com>
2021-01-17 23:04:43 -0500

    Debugging functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,18 +2,18 @@ import * as functions from 'firebase-functions';
 import { WeatherService } from './service/weatherService';
 import * as admin from 'firebase-admin';
 
-admin.initializeApp();
+admin.initializeApp({ databaseURL: "https://myrke-189201.firebaseio.com" });
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+  functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
 
 export const updateWeather = functions.pubsub.schedule('every 120 minutes').onRun(async (context) => {
-  functions.logger.info("Function updateWhether triggered", {structuredData: true});
+  functions.logger.info("Function updateWhether triggered", { structuredData: true });
   const service = new WeatherService();
   await service.updateDetail();
 });
