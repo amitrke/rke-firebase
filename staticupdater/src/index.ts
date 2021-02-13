76e9eafd63d077829c390a89d2bb3be60a2e0574
Amit Kumar <amitrke@gmail.com>
2021-02-13 11:40:40 -0500

    Album static pages

--- staticupdater/src/index.ts
+++ staticupdater/src/index.ts
@@ -1,4 +1,5 @@
 import * as admin from 'firebase-admin';
+import { mkdirSync } from 'fs';
 import { HomepageConfig, RemoteConfigDefaultValueType } from './models/types';
 var serviceAccount = require("../firebase-adminsdk.json");
 var _ = require('lodash');
@@ -6,6 +7,7 @@ const handlebars = require("handlebars");
 const fs = require('fs');
 const Entities = require('html-entities').XmlEntities;
 const entities = new Entities();
+const path = require("path");
 
 var app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
@@ -28,6 +30,12 @@ handlebars.registerPartial('sidebar', sidebar);
 
 async function readPosts() {
   var ref = db.ref("posts");
+  const snapshot = await ref.once('value');
+  return snapshot.val();
+}
+
+async function readPhotos() {
+  var ref = db.ref("album");
   const snapshot = await ref.once('value');
   return snapshot.val();
 }
@@ -69,6 +77,23 @@ function processPosts(posts: any, users: any): any[] {
   return newPosts;
 }
 
+function processPhotos(photos: any, users: any) {
+  const albums:any = {};
+  _.forEach(photos, (userPhotos: any, userId: string) => {
+    _.forEach(userPhotos, (photo: any, photoId: string) => {
+      if (photo.publish == true && photo.album && photo.album.trim() != ""){
+        if (!albums[photo.album]) {
+          albums[photo.album] = [];
+        }
+        photo.id=photoId;
+        photo.userId = userId;
+        albums[photo.album].push(photo);
+      }
+    })
+  })
+  return albums;
+}
+
 async function generatePostFiles(posts: any) {
   const source = fs.readFileSync("./src/templates/post.hbs", 'utf8');
 
@@ -79,16 +104,85 @@ async function generatePostFiles(posts: any) {
     if (post.photoPath) {
       const srcFilename = post.photoPath;
       const destFilename = `../public/posts/${post.id}.jpg`; //Should be extracted from the source path.
-      const options = {
-        destination: destFilename,
-      };
-      // Downloads the file
-      await bucket.file(srcFilename).download(options);
+      await downloadFile(srcFilename, destFilename);
     }
-
     fs.writeFileSync(`../public/posts/${post.htmlFilename}`, result);
   });
 }
+
+async function downloadFile(sourcePath: string, destPath: string) {
+  const options = {
+    destination: destPath,
+  };
+  await bucket.file(sourcePath).download(options);
+}
+
+async function generateAlbumFiles(albums: any) {
+  const source = fs.readFileSync("./src/templates/album.hbs", 'utf8');
+  const template = handlebars.compile(source, { strict: true });
+
+  _.forEach(albums, async (photos: any[], albumName: string) => {
+    console.log(albumName);
+    const albumNameWithDash = albumName.replace(" ", "-");
+    const dirPath = path.join(__dirname, "..", "..", "public", "albums", albumNameWithDash);
+    console.log("Checking for directory " 
+              + dirPath);
+    const folderExists = fs.existsSync(dirPath);
+    console.log(folderExists ? "The directory already exists" 
+                      : "Not found!"); 
+    if (!folderExists){
+      mkdirSync(dirPath);
+    }
+    
+    const photoArray:any[] = [];
+    for (const photo of photos){
+      const srcFilename = photo['filename'] as string;
+      const pathDotSplit = srcFilename.split('.');
+      const fileExtn = pathDotSplit[pathDotSplit.length - 1];
+      const file680path = `users/${photo['userId']}/thumbnails/${pathDotSplit[0]}_680x680.${fileExtn}`;
+      const destFilename = `../public/albums/${albumNameWithDash}/${pathDotSplit[0]}_680x680.jpg`;
+      await downloadFile(file680path, destFilename);
+      photo['file680path'] = `${albumNameWithDash}/${pathDotSplit[0]}_680x680.jpg`;
+      photoArray.push(photo);
+    }
+    const result = template({ albumName, albumNameWithDash, photoArray });
+    fs.writeFileSync(`../public/albums/${albumNameWithDash}.html`, result);
+  })
+
+}
+
+// async function generateAlbumLandingPage(albums: any) {
+//   const source = fs.readFileSync("./src/templates/albumLanding.hbs", 'utf8');
+//   const template = handlebars.compile(source, { strict: true });
+
+//   _.forEach(albums, async (photos: any[], albumName: string) => {
+//     const albumNameWithDash = albumName.replace(" ", "-");
+//     const dirPath = path.join(__dirname, "..", "..", "public", "albums", albumNameWithDash);
+//     console.log("Checking for directory " 
+//               + dirPath);
+//     const folderExists = fs.existsSync(dirPath);
+//     console.log(folderExists ? "The directory already exists" 
+//                       : "Not found!"); 
+//     if (!folderExists){
+//       mkdirSync(dirPath);
+//     }
+    
+//     const photoArray:any[] = [];
+//     for (const photo of photos){
+//       const srcFilename = photo['filename'] as string;
+//       const pathDotSplit = srcFilename.split('.');
+//       const fileExtn = pathDotSplit[pathDotSplit.length - 1];
+//       const file680path = `users/${photo['userId']}/thumbnails/${pathDotSplit[0]}_680x680.${fileExtn}`;
+//       const destFilename = `../public/albums/${albumNameWithDash}/${pathDotSplit[0]}_680x680.jpg`;
+//       await downloadFile(file680path, destFilename);
+//       photo['file680path'] = `${albumNameWithDash}/${pathDotSplit[0]}_680x680.jpg`;
+//       photoArray.push(photo);
+//     }
+//     const result = template({ albumName, albumNameWithDash, photoArray });
+//     fs.writeFileSync(`../public/albums/${albumNameWithDash}.html`, result);
+//   })
+
+// }
 
 async function generateHomepage(posts: any) {
   const source = fs.readFileSync("./src/templates/home.hbs", 'utf8');
@@ -108,9 +202,12 @@ async function generateHomepage(posts: any) {
 
 async function asyncCall() {
   const posts = await readPosts();
+  const photos = await readPhotos();
   const users = await readUsers();
+  const albums = await processPhotos(photos, users);
   const userPosts = processPosts(posts, users);
   await generatePostFiles(userPosts);
+  await generateAlbumFiles(albums);
   await generateHomepage(userPosts);
   app.delete();
 }
